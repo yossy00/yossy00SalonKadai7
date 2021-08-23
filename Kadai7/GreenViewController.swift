b7abb79c834715f3ebfc12f65fa26459bb23e869
吉田晃崇 <yoshidaakitaka@yoshidaakiratakashinoMacBook-Pro.local>
2021-08-23 14:48:29 +0900

    first commit

---
+++ Kadai7/GreenViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GreenViewController.swift
+//  Kadai7
+//
+//  Created by 吉田晃崇 on 2021/08/15.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+
+    @IBOutlet private weak var number1TextField: UITextField!
+    @IBOutlet private weak var number2TextField: UITextField!
+    @IBOutlet private weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        number1TextField.keyboardType = .phonePad
+        number2TextField.keyboardType = .phonePad        
+    }
+    
+    @IBAction func CalculationButton(_ sender: Any) {
+        
+        let num1 = Int(number1TextField.text ?? "") ?? 0
+        let num2 = Int(number2TextField.text ?? "") ?? 0
+        let result = num1 - num2
+        resultLabel.text = String(result)
+        number1TextField.resignFirstResponder()
+        number2TextField.resignFirstResponder()
+    }
+}
